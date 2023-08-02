9526509976c9ca2c55f3b4d9ddbbd082d510d0eb
satara <25549072+satara2358@users.noreply.github.com>
2023-08-01 20:41:01 -0500

    Layout

--- components/Layout.js
+++ components/Layout.js
@@ -3,19 +3,23 @@ import { Sora } from '@next/font/google';
 import  Header  from '../components/Header';
 import TopLeftImg from '../components/TopLeftImg';
 
-
 const sora = Sora({
-  subsets: ['Latin'],
+  subsets: ['latin'],
   variable: '--font-sora',
-  weight: ['100','200','300','400','500','600','700','800']
+  weight: ['100','200','300','400','500','600','700','800'],
 });
 
-const Layout = () => {
+const Layout = ({ children }) => {
   return (
-    <div>
+    <div 
+      className={`page bg-site text-white bg-cover bg-no-repeat 
+      ${sora.variable}
+      font-sora relative`}
+    >
+      <TopLeftImg/>
       <Nav />
       <Header/>
-      <TopLeftImg/>
+      { children }
     </div>
   )
 };
