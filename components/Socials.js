9bb33b0b88926982679ff30f1640783d345d0c06
satara <25549072+satara2358@users.noreply.github.com>
2023-08-04 00:06:40 -0500

    IndexAbout

--- components/Socials.js
+++ components/Socials.js
@@ -1,5 +1,36 @@
+import Link from 'next/link';
+import { RiDiscordLine, RiFacebookBoxLine, RiGithubLine, RiInstagramLine, RiPinterestLine, RiTwitterLine, RiYoutubeLine } from 'react-icons/ri';
+
 const Socials = () => {
-  return <div>Socials</div>;
+  return (
+    <div className='flex items-center gap-x-5 text-lg'>
+      <Link
+        href={''}
+        className='hover:text-accent transition-all duration-300'>
+        <RiYoutubeLine />
+      </Link>
+      <Link
+        href={''}
+        className='hover:text-accent transition-all duration-300'>
+        <RiTwitterLine />
+      </Link>
+      <Link
+        href={''}
+        className='hover:text-accent transition-all duration-300'>
+        <RiFacebookBoxLine />
+      </Link>
+      <Link
+        href={''}
+        className='hover:text-accent transition-all duration-300'>
+        <RiGithubLine />
+      </Link>
+      <Link
+        href={''}
+        className='hover:text-accent transition-all duration-300'>
+        <RiDiscordLine />
+      </Link>
+    </div>
+  )
 };
 
 export default Socials;
--- pages/about/index.js
+++ pages/about/index.js
@@ -1,24 +1,32 @@
 //  data
+import React, { useState } from "react";
+import Avatar from "../../components/Avatar";
+import Circles from '../../components/Circles';
+import { motion } from "framer-motion";
+import { FaCss3, FaFigma, FaHtml5, FaJs, FaReact, FaWordpress } from 'react-icons/fa';
+import { SiAdobephotoshop, SiAdobexd, SiFramer, SiNextdotjs } from 'react-icons/si';
+import { fadeIn } from "../../variants";
+
 const aboutData = [
   {
     title: 'skills',
     info: [
-      // {
-      //   title: 'Web Development',
-      //   icons: [
-      //     <FaHtml5 />,
-      //     <FaCss3 />,
-      //     <FaJs />,
-      //     <FaReact />,
-      //     <SiNextdotjs />,
-      //     <SiFramer />,
-      //     <FaWordpress />,
-      //   ],
-      // },
-      // {
-      //   title: 'UI/UX Design',
-      //   icons: [<FaFigma />, <SiAdobexd />, <SiAdobephotoshop />],
-      // },
+      {
+        title: 'Web Development',
+        icons: [
+          <FaHtml5 />,
+          <FaCss3 />,
+          <FaJs />,
+          <FaReact />,
+          <SiNextdotjs />,
+          <SiFramer />,
+          <FaWordpress />,
+        ],
+      },
+      {
+        title: 'UI/UX Design',
+        icons: [<FaFigma />, <SiAdobexd />, <SiAdobephotoshop />],
+      },
     ],
   },
   {
@@ -71,7 +79,41 @@ const aboutData = [
 ];
 
 const About = () => {
-  return <div>About</div>;
+  const [index, setIndex] = useState(0);
+
+  return (
+    <div className="h-full bg-primary/30 py-32 text-center xl:text-left">
+      <Circles />
+      <motion.div
+        variants={fadeIn('right', 0.2 )} 
+        initial='hidden'
+        animate='show'
+        exit='hidden'
+        className="hidden xl:flex absolute bottom-0 -left-[370px]">
+        <Avatar/>
+      </motion.div>
+      <div className="container mx-auto h-full flex flex-col items-center xl:flex-row gap-x-6">
+        <div>text</div>
+        <div>
+          <div className="flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4">
+            {aboutData.map((item, itemIndex) => {
+              return (
+                <div 
+                  key={itemIndex}
+                  className={`${
+                    index === itemIndex && 'text-accent after:w-[100%] after:bg-accent after:transition-all after:duration-300'
+                  } cursor-pointer capitalize xl:text-lg relative after:w-8 after:h-[2px] after:bg-white after:absolute after:bottom-1 after:left-0`}
+                  onClick={() => setIndex(itemIndex)}
+                >
+                  {item.title}
+                </div>
+              )
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 };
 
 export default About;
