82d004b401d24279e8032c1a3f5ce2dcb54e50fc
satara <25549072+satara2358@users.noreply.github.com>
2023-08-03 22:36:52 -0500

    HeaderLogo

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,27 @@
+
+import Link from 'next/link';
+import Socials from '../components/Socials';
+import Image from 'next/image';
+
 const Header = () => {
   return (
     <header
-      className="absolute z-30 w-full"
-    >Header
+      className="absolute z-30 w-full flex items-center px-16 xl:px-0 xl:h-[90px]"
+    >
+      <div className='container mx-auto'>
+        <div>
+          <Link href={'/'}>
+            <Image 
+              src={'/logo.svg'}
+              width={220}
+              height={48}
+              alt=''
+              priority={true}
+            />
+          </Link>
+          
+        </div>
+      </div>
     </header>
   )
 };
