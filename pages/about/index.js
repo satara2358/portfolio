f02fb3a4a2a878756898ec5a24e50387d4f5b598
satara <25549072+satara2358@users.noreply.github.com>
2023-08-06 00:30:18 -0500

    UpdateAbout

--- pages/about/index.js
+++ pages/about/index.js
@@ -95,28 +95,74 @@ const About = () => {
       </motion.div>
       <div className="container mx-auto h-full flex flex-col items-center xl:flex-row gap-x-6">
         <div className="flex-1 flex flex-col justify-center">
-          <h2 className="h2">
+          <motion.h2 
+            variants={fadeIn('right', 0.2 )} 
+            initial='hidden'
+            animate='show'
+            exit='hidden'
+            className="h2">
             Capting 
             <span className="text-accent">
               desing
             </span>
-          </h2>
-          <p className="max-w-[500px] mx-auto xl:mx-0 mb-6 xl:mb-12 px-2 xl:px-0">
+          </motion.h2>
+          <p
+            variants={fadeIn('right', 0.4 )} 
+            initial='hidden'
+            animate='show'
+            exit='hidden' 
+            className="max-w-[500px] mx-auto xl:mx-0 mb-6 xl:mb-12 px-2 xl:px-0">
             Open http://localhost:3000 with your browser to see the result. You can start editing he page by modifying pages/index.js. The page auto-updates as you edit the file. API routes can be accessed on http://localhost:3000/api/hello. This endpoint can be edited in pages/api/hello.js.
             The pages/api directory is mapped to /api/*. Files in this directory are treated as API routes instead of React pages.
           </p>
-          <div>
-            <div>
+          <motion.div 
+            variants={fadeIn('right', 0.6)} 
+            initial='hidden'
+            animate='show'
+            exit='hidden'
+            className="hidden md:flex md:max-w-xl xl:max-w-none mx-auto xl:mx-0 mb-8">
+            <div className="flex flex-1 xl:gap-x-6">
               <div className="relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0">
                 <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2">
-                  <CountUp start={0} end={10} duration={10}/>
+                  <CountUp start={0} end={10} duration={10}/>+
                 </div>
-                <div>Años de Experiencia</div>
+                <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]">
+                  Años de Experiencia
+                </div>
+              </div>
+              <div className="relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0">
+                <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2">
+                  <CountUp start={0} end={250} duration={5}/>+
+                </div>
+                <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]">
+                  Clientes Satisfechos
+                </div>
+              </div>
+              <div className="relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0">
+                <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2">
+                  <CountUp start={0} end={650} duration={5}/>+
+                </div>
+                <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]">
+                  Proyectos Finalizados
+                </div>
+              </div>
+              <div className="relative flex-1">
+                <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2">
+                  <CountUp start={0} end={8} duration={5}/>+
+                </div>
+                <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]">
+                  Premios de Diseño
+                </div>
               </div>
             </div>
-          </div>
+          </motion.div>
         </div>
-        <div className=" flex flex-col w-full xl:max-w-[48%] h-[480px]">
+        <motion.div 
+          variants={fadeIn('left', 0.4)} 
+          initial='hidden'
+          animate='show'
+          exit='hidden'
+          className=" flex flex-col w-full xl:max-w-[48%] h-[480px]">
           <div className="flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4">
             {aboutData.map((item, itemIndex) => {
               return (
@@ -155,7 +201,7 @@ const About = () => {
               )
             })}
           </div>
-        </div>
+        </motion.div>
       </div>
     </div>
   )
