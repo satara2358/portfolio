53194f4237dbe128e76e1b536e3a41fb84db33c6
satara <25549072+satara2358@users.noreply.github.com>
2023-08-10 00:17:29 -0500

    Update

--- components/TestimonialSlider.js
+++ components/TestimonialSlider.js
@@ -2,9 +2,9 @@
 
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
-import 'swiper/css/free-mode';
+import 'swiper/css/navigation';
 import 'swiper/css/pagination';
-import { Pagination } from 'swiper';
+import { Pagination, Navigation } from 'swiper';
 import Image from 'next/image';
 import { FaQuoteLeft } from 'react-icons/fa'
 
@@ -35,29 +35,35 @@ const testimonialData = [
 const TestimonialSlider = () => {
   return (
     <Swiper
-      spaceBetween={10}
+      navigation={true}
       pagination={{
         clickable: true,
       }}
-      modules={[Pagination]}
-      className='h-[280px] sm:h-[480px]'
+      modules={[Navigation, Pagination]}
+      className='h-[400px]'
     >
       {testimonialData.map((person, index) =>{
         return(
           <SwiperSlide key={index}>
-            <div>
-              <div>
-                <div>
-                  <div>Avatar</div>
-                  <div>Avatar</div>
-                  <div>Avatar</div>
+            <div className='flex flex-col items-center md:flex-row gap-x-8 h-full px-16'>
+              <div className='w-full max-w-[300px] flex flex-col xl:justify-center items-center relative mx-auto xl:mx-0'>
+                <div className='flex flex-col justify-center text-center'>
+                  <div className='mb-2 mx-auto'>
+                    <Image src={person.image} width={100} height={100} alt=''/>
+                  </div>
+                  <div className='text-lg'>
+                    {person.name}
+                  </div>
+                  <div className='text-[12px] uppercase font-extralight tracking-widest'>
+                    {person.position}
+                  </div>
                 </div>
               </div>
-              <div className='bg-pink-500/10 flex-1 flex flex-col justify-centerbefore:w-[1px] xl:before:bg-white/20 xl:before:absolute xl:absolute:left-0 xl:before:h-[200px] relative xl:pl-20'>
+              <div className='flex-1 flex flex-col justify-centerbefore:w-[1px] xl:before:bg-white/20 xl:before:absolute xl:absolute:left-0 xl:before:h-[200px] relative xl:pl-20'>
                 <div className='mb-4'>
                   <FaQuoteLeft className='text-4xl xl:text-6xl text-white/20 mx-auto md:mx-0'/>
                 </div>
-                <div>
+                <div className='xl:text-lg text-center md:text-left'>
                   {person.message}
                 </div>
               </div>
